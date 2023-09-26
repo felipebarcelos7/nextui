1fbd7002d83f02ce5a454b7372ac197200ad0107
Felipe Barcelos <75994694+felipebarcelos7@users.noreply.github.com>
2023-09-26 16:39:06 -0300

    Update card.stories.tsx

--- packages/components/card/stories/card.stories.tsx
+++ packages/components/card/stories/card.stories.tsx
@@ -253,6 +253,198 @@ const CoverImgTemplate = (args: CardProps) => (
         </div>
         <Button radius="full">Get App</Button>
       </CardFooter>
+    </Card><Card {...args} isFooterBlurred className="w-full h-[400px] col-span-12 sm:col-span-7">
+      <CardHeader className="absolute z-10 top-1 flex-col items-start">
+        <p className="text-xs text-white/60 uppercase font-bold">Your day your way</p>
+        <h4 className="text-white/90 font-medium text-2xl">Your checklist for better sleep</h4>
+      </CardHeader>
+      <img
+        alt="Relaxing app background"
+        className="w-full h-full object-cover"
+        src="https://nextui.org/images/card-example-5.jpeg"
+      />
+      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t border-default-600 dark:border-default-100">
+        <div className="flex flex-grow gap-2 items-center">
+          <img
+            alt="Breathing app icon"
+            className="rounded-full w-10 h-11 bg-black"
+            src={require("./assets/breathing-app-icon.jpeg")}
+          />
+          <div className="flex flex-col">
+            <p className="text-xs text-white/60">Breathing App</p>
+            <p className="text-xs text-white/60">Get a good night&apos;s sleep.</p>
+          </div>
+        </div>
+        <Button radius="full">Get App</Button>
+      </CardFooter>
+    </Card><Card {...args} isFooterBlurred className="w-full h-[400px] col-span-12 sm:col-span-7">
+      <CardHeader className="absolute z-10 top-1 flex-col items-start">
+        <p className="text-xs text-white/60 uppercase font-bold">Your day your way</p>
+        <h4 className="text-white/90 font-medium text-2xl">Your checklist for better sleep</h4>
+      </CardHeader>
+      <img
+        alt="Relaxing app background"
+        className="w-full h-full object-cover"
+        src="https://nextui.org/images/card-example-5.jpeg"
+      />
+      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t border-default-600 dark:border-default-100">
+        <div className="flex flex-grow gap-2 items-center">
+          <img
+            alt="Breathing app icon"
+            className="rounded-full w-10 h-11 bg-black"
+            src={require("./assets/breathing-app-icon.jpeg")}
+          />
+          <div className="flex flex-col">
+            <p className="text-xs text-white/60">Breathing App</p>
+            <p className="text-xs text-white/60">Get a good night&apos;s sleep.</p>
+          </div>
+        </div>
+        <Button radius="full">Get App</Button>
+      </CardFooter>
+    </Card><Card {...args} isFooterBlurred className="w-full h-[400px] col-span-12 sm:col-span-7">
+      <CardHeader className="absolute z-10 top-1 flex-col items-start">
+        <p className="text-xs text-white/60 uppercase font-bold">Your day your way</p>
+        <h4 className="text-white/90 font-medium text-2xl">Your checklist for better sleep</h4>
+      </CardHeader>
+      <img
+        alt="Relaxing app background"
+        className="w-full h-full object-cover"
+        src="https://nextui.org/images/card-example-5.jpeg"
+      />
+      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t border-default-600 dark:border-default-100">
+        <div className="flex flex-grow gap-2 items-center">
+          <img
+            alt="Breathing app icon"
+            className="rounded-full w-10 h-11 bg-black"
+            src={require("./assets/breathing-app-icon.jpeg")}
+          />
+          <div className="flex flex-col">
+            <p className="text-xs text-white/60">Breathing App</p>
+            <p className="text-xs text-white/60">Get a good night&apos;s sleep.</p>
+          </div>
+        </div>
+        <Button radius="full">Get App</Button>
+      </CardFooter>
+    </Card><Card {...args} isFooterBlurred className="w-full h-[400px] col-span-12 sm:col-span-7">
+      <CardHeader className="absolute z-10 top-1 flex-col items-start">
+        <p className="text-xs text-white/60 uppercase font-bold">Your day your way</p>
+        <h4 className="text-white/90 font-medium text-2xl">Your checklist for better sleep</h4>
+      </CardHeader>
+      <img
+        alt="Relaxing app background"
+        className="w-full h-full object-cover"
+        src="https://nextui.org/images/card-example-5.jpeg"
+      />
+      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t border-default-600 dark:border-default-100">
+        <div className="flex flex-grow gap-2 items-center">
+          <img
+            alt="Breathing app icon"
+            className="rounded-full w-10 h-11 bg-black"
+            src={require("./assets/breathing-app-icon.jpeg")}
+          />
+          <div className="flex flex-col">
+            <p className="text-xs text-white/60">Breathing App</p>
+            <p className="text-xs text-white/60">Get a good night&apos;s sleep.</p>
+          </div>
+        </div>
+        <Button radius="full">Get App</Button>
+      </CardFooter>
+    </Card><Card {...args} isFooterBlurred className="w-full h-[400px] col-span-12 sm:col-span-7">
+      <CardHeader className="absolute z-10 top-1 flex-col items-start">
+        <p className="text-xs text-white/60 uppercase font-bold">Your day your way</p>
+        <h4 className="text-white/90 font-medium text-2xl">Your checklist for better sleep</h4>
+      </CardHeader>
+      <img
+        alt="Relaxing app background"
+        className="w-full h-full object-cover"
+        src="https://nextui.org/images/card-example-5.jpeg"
+      />
+      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t border-default-600 dark:border-default-100">
+        <div className="flex flex-grow gap-2 items-center">
+          <img
+            alt="Breathing app icon"
+            className="rounded-full w-10 h-11 bg-black"
+            src={require("./assets/breathing-app-icon.jpeg")}
+          />
+          <div className="flex flex-col">
+            <p className="text-xs text-white/60">Breathing App</p>
+            <p className="text-xs text-white/60">Get a good night&apos;s sleep.</p>
+          </div>
+        </div>
+        <Button radius="full">Get App</Button>
+      </CardFooter>
+    </Card><Card {...args} isFooterBlurred className="w-full h-[400px] col-span-12 sm:col-span-7">
+      <CardHeader className="absolute z-10 top-1 flex-col items-start">
+        <p className="text-xs text-white/60 uppercase font-bold">Your day your way</p>
+        <h4 className="text-white/90 font-medium text-2xl">Your checklist for better sleep</h4>
+      </CardHeader>
+      <img
+        alt="Relaxing app background"
+        className="w-full h-full object-cover"
+        src="https://nextui.org/images/card-example-5.jpeg"
+      />
+      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t border-default-600 dark:border-default-100">
+        <div className="flex flex-grow gap-2 items-center">
+          <img
+            alt="Breathing app icon"
+            className="rounded-full w-10 h-11 bg-black"
+            src={require("./assets/breathing-app-icon.jpeg")}
+          />
+          <div className="flex flex-col">
+            <p className="text-xs text-white/60">Breathing App</p>
+            <p className="text-xs text-white/60">Get a good night&apos;s sleep.</p>
+          </div>
+        </div>
+        <Button radius="full">Get App</Button>
+      </CardFooter>
+    </Card><Card {...args} isFooterBlurred className="w-full h-[400px] col-span-12 sm:col-span-7">
+      <CardHeader className="absolute z-10 top-1 flex-col items-start">
+        <p className="text-xs text-white/60 uppercase font-bold">Your day your way</p>
+        <h4 className="text-white/90 font-medium text-2xl">Your checklist for better sleep</h4>
+      </CardHeader>
+      <img
+        alt="Relaxing app background"
+        className="w-full h-full object-cover"
+        src="https://nextui.org/images/card-example-5.jpeg"
+      />
+      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t border-default-600 dark:border-default-100">
+        <div className="flex flex-grow gap-2 items-center">
+          <img
+            alt="Breathing app icon"
+            className="rounded-full w-10 h-11 bg-black"
+            src={require("./assets/breathing-app-icon.jpeg")}
+          />
+          <div className="flex flex-col">
+            <p className="text-xs text-white/60">Breathing App</p>
+            <p className="text-xs text-white/60">Get a good night&apos;s sleep.</p>
+          </div>
+        </div>
+        <Button radius="full">Get App</Button>
+      </CardFooter>
+    </Card><Card {...args} isFooterBlurred className="w-full h-[400px] col-span-12 sm:col-span-7">
+      <CardHeader className="absolute z-10 top-1 flex-col items-start">
+        <p className="text-xs text-white/60 uppercase font-bold">Your day your way</p>
+        <h4 className="text-white/90 font-medium text-2xl">Your checklist for better sleep</h4>
+      </CardHeader>
+      <img
+        alt="Relaxing app background"
+        className="w-full h-full object-cover"
+        src="https://nextui.org/images/card-example-5.jpeg"
+      />
+      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t border-default-600 dark:border-default-100">
+        <div className="flex flex-grow gap-2 items-center">
+          <img
+            alt="Breathing app icon"
+            className="rounded-full w-10 h-11 bg-black"
+            src={require("./assets/breathing-app-icon.jpeg")}
+          />
+          <div className="flex flex-col">
+            <p className="text-xs text-white/60">Breathing App</p>
+            <p className="text-xs text-white/60">Get a good night&apos;s sleep.</p>
+          </div>
+        </div>
+        <Button radius="full">Get App</Button>
+      </CardFooter>
     </Card>
   </div>
 );
